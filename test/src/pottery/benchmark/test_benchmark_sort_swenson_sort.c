217f644385eb9dbed95f64d08def3cd65b4e4f45
Nicholas Fraser <nick@ludocode.com>
2021-01-05 01:39:48 -0500

    Update copyright

--- test/src/pottery/benchmark/test_benchmark_sort_swenson_sort.c
+++ test/src/pottery/benchmark/test_benchmark_sort_swenson_sort.c
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2020 Nicholas Fraser
+ * Copyright (c) 2020-2021 11037650 Canada Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
